5bd79a6bfc9ad2d8b3c19c145dd32014a7752721
XX <freecoder.xx@gmail.com>
2018-04-27 15:59:25 +0300

    Add tests for all param in list #10

--- tests/list.rs
+++ tests/list.rs
@@ -82,17 +82,56 @@ target/test_list/issues/new/task2.md
         ] => ""
     );
 
+    create_file!("target/test_list/issues/.fin/task0.md", "");
+    create_file!("target/test_list/issues/.task3.md", "");
+
+    assert_output!(
+        [
+            "todo list",
+            "todo list all:false",
+            "todo list a:f",
+            "todo --list",
+            "todo --list all:not",
+            "todo --list a:no",
+            "todo -l all:f",
+            "todo -l a:n",
+            "todo -l a:0"
+        ] => r#"
+target/test_list/issues/task1.md
+target/test_list/issues/new/task2.md
+"#
+    );
+
+    assert_output!(
+        [
+            "todo list a:",
+            "todo --list all:",
+            "todo -l a:t",
+            "todo -l all:+"
+        ] => r#"
+target/test_list/issues/task1.md
+target/test_list/issues/new/task2.md
+target/test_list/issues/.fin/task0.md
+target/test_list/issues/.task3.md
+"#
+    );
+
     delete_file!("target/test_list/issues/task1.md");
     delete_file!("target/test_list/issues/new/task2.md");
+    delete_file!("target/test_list/issues/.fin/task0.md");
+    delete_file!("target/test_list/issues/.task3.md");
 
     assert_output!(
         [
             "todo list",
             "todo list task1",
+            "todo list all:",
             "todo --list",
             "todo --list task1",
+            "todo --list a:true",
             "todo -l",
-            "todo -l task1"
+            "todo -l task1",
+            "todo -l a:"
         ] => ""
     );
 }
